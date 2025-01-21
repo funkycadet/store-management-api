30d0d32914603a6dc53138cfb854a77b5a468cd2
Eric Alaribe <ericsmithsonian2@gmail.com>
2025-01-21 16:26:50 +0100

    task: add routes for product

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import authRouter from "./auth.routes";
 import userRouter from "./user.routes";
 import healthRouter from "./healthCheck.routes";
+import productRouter from "./product.routes";
 
-export { authRouter, healthRouter, userRouter };
+export { authRouter, healthRouter, userRouter, productRouter };
